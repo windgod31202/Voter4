6fc679d17830835ee7332d458bd0d1b07a785124
JaychenWG <jay0345678@gmail.com>
2022-04-25 04:03:56 +0800

    voter

--- voter4.v
+++ voter4.v
@@ -4,12 +4,12 @@ output [3:1] O; // O Result
 reg [3:1] O;
     always@ (I)
         case (I)
-            4'b0000:O[3] = 1;
-            4'b0001:O[3] = 1;
-            4'b0010:O[3] = 1;
-            4'b0100:O[3] = 1;
-            4'b1000:O[3] = 1;
-            default:O[3] = 0;
+            4'b0000:O[1] = 1;
+            4'b0001:O[1] = 1;
+            4'b0010:O[1] = 1;
+            4'b0100:O[1] = 1;
+            4'b1000:O[1] = 1;
+            default:O[1] = 0;
         endcase
     always @(I) begin
         case (I)
@@ -24,12 +24,12 @@ reg [3:1] O;
     end
     always @(I) begin
         case (I)
-            4'b1110:O[1] = 1;
-            4'b1101:O[1] = 1;
-            4'b1011:O[1] = 1;
-            4'b0111:O[1] = 1;
-            4'b1111:O[1] = 1;
-            default:O[1] = 0; 
+            4'b1110:O[3] = 1;
+            4'b1101:O[3] = 1;
+            4'b1011:O[3] = 1;
+            4'b0111:O[3] = 1;
+            4'b1111:O[3] = 1;
+            default:O[3] = 0; 
         endcase
     end
 endmodule
